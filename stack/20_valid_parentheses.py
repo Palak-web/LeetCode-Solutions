f315fbc137c6b3432fd8670cb068d51e86fbe171
Palak-web <palak55544@gmail.com>
2026-07-12 00:01:52 +0530

    Add: Valid Parentheses (Stack) - LeetCode #20

---
+++ stack/20_valid_parentheses.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        pairs = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in '({[':
+                stack.append(char)
+            else:
+                if not stack or stack.pop() != pairs[char]:
+                    return False
+        
+        return not stack
