c7479dc37241fc884dd04b55d1b1a3ba4f26d209
Palak-web <palak55544@gmail.com>
2026-07-15 23:59:08 +0530

    Add README files for root, Arrays, and Stack directories

--- Arrays/26-remove_duplicates_arr/solution.py
+++ Arrays/26-remove_duplicates_arr/solution.py
@@ -8,3 +8,18 @@ class Solution(object):
                 i += 1
 
         return i
+    """
+# 26. Remove Duplicates from Sorted Array
+
+## Approach
+Use two pointers:
+- `i` stores the position of the next unique element.
+- `j` traverses the array.
+- When a new unique element is found, place it at index `i`.
+
+## Time Complexity
+O(n)
+
+## Space Complexity
+O(1)
+    """
---
+++ stack/README.md
@@ -0,0 +1,13 @@
+# Stack
+
+This folder contains my Python solutions to LeetCode stack problems.
+
+## Topics to be Covered
+
+* Basic Stack Operations
+* Monotonic Stack
+* Parentheses Matching
+* Expression Evaluation
+* Next Greater Element
+
+More solutions will be added as I continue practicing Data Structures and Algorithms.
